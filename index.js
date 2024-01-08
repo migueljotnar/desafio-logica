d2e83592c50d8a8034aa9f76b1b1c99dd05ef1be
migueljotnar <arquivos.docluiz@gmail.com>
2024-01-08 12:12:14 -1000

    Projeto inicial de logica

---
+++ index.js
@@ -0,0 +1,19 @@
+let nickname = "Miguel"
+let userXp = 30000
+if (userXp <= 1000){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Ferro!")
+}else if(userXp <= 2000){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Bronze!")
+}else if(userXp <= 5000){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Prata!")
+}else if(userXp <= 7000){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Ouro!")
+}else if(userXp <= 8000){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Platina!")
+}else if(userXp <= 9000){
+    cconsole.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Ascendente!")
+}else if(userXp <= 10000){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Imortal!")
+}else if(userXp >= 10001){
+    console.log("O Heroi de nome " + nickname + " esta no nivel de "+ "Radiante!")
+} 
